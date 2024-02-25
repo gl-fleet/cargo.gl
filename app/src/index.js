01ea8446ea705244726ba340a5d48769f0e587b2
Gantulga-kh <gantulga.khuyag@gmail.com>
2024-02-25 18:18:54 +0800

    Added Mobile:Scale

--- app/src/index.js
+++ app/src/index.js
@@ -6,6 +6,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './home'
 import NotFound from './NotFound'
 
+const low = document.documentElement.clientWidth < 768? '0.75' : '1'
+const meta = document.createElement('meta')
+meta.name = "viewport"
+meta.content = `width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=${low}, minimum-scale=${low}`
+document.getElementsByTagName('head')[0].appendChild(meta)
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
